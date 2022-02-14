b74d1e2127afeddd15393690cb13d7b4a4c7c7fa
stfDeveloper <sstefancosmin@icloud.com>
2022-02-14 21:30:42 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+$censura = $_GET["dolor"];
+$leng = strlen($paragrafo);
+$paragrafo = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque aliquam perferendis voluptatem quos architecto numquam suscipit nisi beatae recusandae sapiente!";
+$valoreCensurato = "***";
+
+$paragrafo = str_replace($censura,$valoreCensurato,$paragrafo);
+
+?>
+
+<h1>
+    <?php echo $paragrafo ?>
+</h1>
+<h2>
+    lunghezza:<?php echo $leng ?>
+</h2>
